3ec6e9171ff53f8faae32b0b9ef6f10cbfcd2beb
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-02-15 23:51:50 +0900

    refactor: ListViewを細分化し、条件分岐の可読性を向上

--- GitHubSearchRepositories/View/Content/RepositoryList.swift
+++ GitHubSearchRepositories/View/Content/RepositoryList.swift
@@ -8,44 +8,61 @@ struct RepositoryList: View {
         VStack {
             if let repositories = viewModel.repositories {
                 if !repositories.isEmpty {
-                    List {
-                        Section(content: {
-                            ForEach(viewModel.repositories ?? [], id: \.id) { repository in
-                                RepositoryCell(
-                                    viewModel: viewModel,
-                                    repository: repository
-                                )
-                            }
-                        }, header: {
-                            VStack(alignment: .leading) {
-                                if let searchDetailText = viewModel.searchDetailText {
-                                    Text(searchDetailText)
-                                }
-                                if let totalCount = viewModel.totalCount {
-                                    Text("\(totalCount)件のレポジトリ")
-                                }
-                            }
-                            .font(.caption)
-                            .foregroundStyle(.secondary)
-                        })
-                    }
-                    .scrollDismissesKeyboard(.interactively)
+                    ListView(viewModel: viewModel)
                 } else {
-                    VStack(spacing: 8) {
-                        Spacer()
-                        Image(systemName: "doc.text")
-                            .font(.largeTitle)
-                        Text("該当するレポジトリが見つかりませんでした。")
-                            .font(.headline)
-                        Spacer()
-                    }
-                    .bold()
-                    .foregroundStyle(.gray)
+                    // []（レポジトリが見つからなかった時）
+                    NotFoundView()
                 }
             } else {
+                // nil（初回検索前）
                 Spacer()
             }
         }
+    }
+}
+
+private struct ListView: View {
+    
+    @ObservedObject var viewModel: ContentViewModel
+    
+    var body: some View {
+        List {
+            Section(content: {
+                ForEach(viewModel.repositories ?? [], id: \.id) { repository in
+                    RepositoryCell(
+                        viewModel: viewModel,
+                        repository: repository
+                    )
+                }
+            }, header: {
+                VStack(alignment: .leading) {
+                    if let searchDetailText = viewModel.searchDetailText {
+                        Text(searchDetailText)
+                    }
+                    if let totalCount = viewModel.totalCount {
+                        Text("\(totalCount)件のレポジトリ")
+                    }
+                }
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            })
+        }
+        .scrollDismissesKeyboard(.interactively)
+    }
+}
+
+private struct NotFoundView: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            Spacer()
+            Image(systemName: "doc.text")
+                .font(.largeTitle)
+            Text("該当するレポジトリが見つかりませんでした。")
+                .font(.headline)
+            Spacer()
+        }
+        .bold()
+        .foregroundStyle(.gray)
     }
 }
 
