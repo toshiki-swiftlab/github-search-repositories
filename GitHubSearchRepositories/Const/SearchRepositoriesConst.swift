c95399ae49e27f5e8d62abc7e0890d35fee5256b
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-02-15 18:41:32 +0900

    refactor: APIレスポンスの情報を利用する方針に変更し、不要となったカスタムエラーメッセージを削除

--- GitHubSearchRepositories/Const/SearchRepositoriesConst.swift
+++ GitHubSearchRepositories/Const/SearchRepositoriesConst.swift
@@ -8,15 +8,5 @@ enum SearchRepositoriesConst {
         case notModified = 304
         case validationFailed = 422
         case serviceUnavailable = 503
-        
-        var message: String {
-            switch self {
-            case .ok: return ""
-            case .notModified: return ""
-            case .validationFailed: return "クエリが無効です。"
-            case .serviceUnavailable: return "GitHubがメンテナンス中か、混み合っています。時間を置いてお試しください。"
-            default: return "予期せぬエラーが発生しました。"
-            }
-        }
     }
 }
